bbd8ee1ed716f7c1e813c68aa94e4cb2c028f2f4
SebasTC31 <juansebas0464@gmail.com>
2023-05-21 15:51:26 -0500

    DefaultConnection and DataBaseContext add

---
+++ ConcertWebApi/ConcertWebApi/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using ConcertWebApi.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+namespace ConcertWebApi.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(s => s.EntranceGate).IsUnique();
+        }
+    }
+}
--- ConcertWebApi/ConcertWebApi/Program.cs
+++ ConcertWebApi/ConcertWebApi/Program.cs
@@ -1,3 +1,6 @@
+using ConcertWebApi.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
