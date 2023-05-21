a061913aa77b9a940148d124d9fcdc3cf8367936
SebasTC31 <juansebas0464@gmail.com>
2023-05-21 18:31:47 -0500

    Add TicketsController and SeederDb was configured

---
+++ ConcertWebApi/ConcertWebApi/Controllers/TicketsController.cs
@@ -0,0 +1,68 @@
+﻿using ConcertWebApi.DAL;
+using ConcertWebApi.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace ConcertWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public TicketsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        //all tickets
+        [HttpGet, ActionName("Get")]
+        [Route("Get")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+        {
+            var tickets = await _context.Tickets.ToListAsync();
+
+            if (tickets == null) return NotFound();
+
+            return tickets;
+        }
+
+        //ticket by id
+        [HttpGet, ActionName("Get")]
+        [Route("Get/{id}")]
+        public async Task<ActionResult<Ticket>> GetTicketById(Guid? id)
+        {
+            var ticket = await _context.Tickets.FirstOrDefaultAsync(t => t.Id == id);
+
+            if (ticket == null) return NotFound();
+
+            return Ok(ticket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ValidateTicket(Guid? id)
+        {
+            var ticket = _context.Tickets.Find(id);
+
+            if (ticket == null)
+            {
+                return NotFound("Boleta no válida");
+            }
+            else if (ticket.IsUsed)
+            {
+                return BadRequest($"Boleta ya usada. Fecha: {ticket.UseDate}, portería {ticket.EntranceGate}");
+            }
+            else
+            {
+                ticket.IsUsed = true;
+                ticket.UseDate = DateTime.Now;
+                ticket.EntranceGate = "Sur";
+                await _context.SaveChangesAsync();
+                return Ok("Boleta válida, puede ingresar al concierto");
+            }
+        }
+    }
+}
---
+++ ConcertWebApi/ConcertWebApi/DAL/SeederDb.cs
@@ -0,0 +1,35 @@
+﻿
+using ConcertWebApi.DAL.Entities;
+
+namespace ConcertWebApi.DAL
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+        public SeederDb(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await PopulateTicketsAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task PopulateTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+
+                for (int i = 0; i < 50000; i++)
+                {
+
+                    _context.Tickets.Add(new Ticket { IsUsed = false });
+                }
+            }
+        }
+    }
+}
