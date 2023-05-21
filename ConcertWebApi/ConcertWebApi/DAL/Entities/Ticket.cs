15f811d3fbf320af2942e33c1dcd29537b72c733
SebasTC31 <juansebas0464@gmail.com>
2023-05-21 15:27:56 -0500

    New project ConcertWebApi created with new Ticket Entity

---
+++ ConcertWebApi/ConcertWebApi/DAL/Entities/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertWebApi.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTime? UseDate { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Display(Name = "Ticket use")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Entrace Gate")]
+        [MaxLength(10, ErrorMessage = "The field {0} must have maximun {1} characters")]
+        public string? EntranceGate { get; set; }
+    }
+}
