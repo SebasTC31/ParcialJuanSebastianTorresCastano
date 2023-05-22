f71472076bee17db916186c6cb0d8454e5d8452d
SebasTC31 <juansebas0464@gmail.com>
2023-05-21 21:07:10 -0500

    New project WebPages added with Index action

---
+++ ConcertWebApi/WebPagesAPI/WebPages/Controllers/TicketsController.cs
@@ -0,0 +1,26 @@
+﻿using ConcertWebApi.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebPages.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public TicketsController(IHttpClientFactory httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var url = "https://localhost:7225/api/Tickets/Get";
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Ticket> tickets = JsonConvert.DeserializeObject<List<Ticket>>(json);
+            return View(tickets);
+        }
+    }
+}
